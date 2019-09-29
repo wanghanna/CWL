f935c7b8dd696567372d5af6809e76e0184eb756
wanghanna <hannawang0306@livecom>
2019-09-29 14:56:44 -0700

    修改navbar

--- src/components/navbar.vue
+++ src/components/navbar.vue
@@ -2,35 +2,37 @@
   <div>
     <vs-navbar
       v-model="activeItem"
-      class="nabarx px-3"
-      color="rgba(37, 36, 57, 0.8)"
-      text-color="rgba(255,255,255,.6)"
+      class="nabarx p-1 row m-0"
+      color=""
+      text-color="#989BAC"
       active-text-color="rgba(255,255,255,1)"
       index="danger"
+      id="navbar-t"
     >
-      <div slot="title">
-        <vs-navbar-title id="logo">
+      <div slot="title" class=title>
+        <vs-navbar-title>
           <router-link to="/"
-            ><img
+            ><img id="logo-t"
               src="../assets/LOGO_Lamp.svg"
               width="50vh"
-              class="mt-1"/></router-link
+              class="mt-1 d-none d-md-inline-flex"/></router-link
           >&nbsp;
-          <span style="color:#ffffff" class="d-none d-md-inline-flex">ChenWeiLighting</span>
+          <span style="color:#aaa">ChenWeiLighting</span>
         </vs-navbar-title>
       </div>
+      <div id="menu" class="row m-0">
       <vs-navbar-item index="0">
-        <router-link to="/idx" style="font-size:15px">首頁</router-link>
+        <router-link to="/idx" style="font-size:15px">Home</router-link>
       </vs-navbar-item>
       <vs-navbar-item index="1">
-        <router-link to="/about" style="font-size:15px">關於辰偉</router-link>
+        <router-link to="/about" style="font-size:15px">About US</router-link>
       </vs-navbar-item>
       <vs-navbar-item index="2">
-        <router-link to="/light" style="font-size:15px">產品介紹</router-link>
+        <router-link to="/light" style="font-size:15px">Products</router-link>
       </vs-navbar-item>
       <vs-navbar-item index="3">
         <router-link to="/knowledge" style="font-size:15px"
-          >了解光源</router-link
+          >About Light</router-link
         >
       </vs-navbar-item>
             <vs-navbar-item index="4">
@@ -38,19 +40,41 @@
           >測試頁面</router-link
         >
       </vs-navbar-item>
-      
+      </div>
     </vs-navbar>
   </div>
 </template>
 <style scoped>
-* {
+#title {
   font-family: "Noto Sans TC", sans-serif;
 }
+#menu{
+  font-family: 'Syncopate', sans-serif;
+  font-weight:bolder;
+  padding:10px;
+  border-radius:20px;
+  color:#989BAC;
+}
+#navbar-t{
+}
+
+#logo-t{
+  width:60px;
+  height:60px;
+  background:#0E0F14;
+  /* box-shadow:inset 0 0 10px rgb(146, 146, 146); */
+  padding:10px;
+  border-radius:50%;
+  transition:5s;
+}
+#logo-t:hover{
+
+}
 a:visited {
-  color: #ebebeb;
+  color:"#55586B"
 }
 a:hover {
-  color: #ffffff;
+  color: #0E0F14;
 }
 </style>
 <script>
