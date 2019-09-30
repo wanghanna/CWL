f66f309462dc7a86a13c7b8b960ddd6095839e28
wanghanna <hannawang0306@livecom>
2019-09-29 19:10:16 -0700

    暫時修改

--- src/components/navbar.vue
+++ src/components/navbar.vue
@@ -1,9 +1,9 @@
 <template>
-  <div>
+  <div class="container-fluid m-0 p-0">
     <vs-navbar
       v-model="activeItem"
       class="nabarx p-1 row m-0"
-      color=""
+      color="rgba(255,255,255,0.1)"
       text-color="#989BAC"
       active-text-color="rgba(255,255,255,1)"
       index="danger"
@@ -14,29 +14,28 @@
           <router-link to="/"
             ><img id="logo-t"
               src="../assets/LOGO_Lamp.svg"
-              width="50vh"
               class="mt-1 d-none d-md-inline-flex"/></router-link
           >&nbsp;
-          <span style="color:#aaa">ChenWeiLighting</span>
+          <span style="color:yellow">ChenWeiLighting</span>
         </vs-navbar-title>
       </div>
       <div id="menu" class="row m-0">
       <vs-navbar-item index="0">
-        <router-link to="/idx" style="font-size:15px">Home</router-link>
+        <router-link to="/idx" style="font-size:12px">Home</router-link>
       </vs-navbar-item>
       <vs-navbar-item index="1">
-        <router-link to="/about" style="font-size:15px">About US</router-link>
+        <router-link to="/about" style="font-size:12px">About US</router-link>
       </vs-navbar-item>
       <vs-navbar-item index="2">
-        <router-link to="/light" style="font-size:15px">Products</router-link>
+        <router-link to="/light" style="font-size:12px">Products</router-link>
       </vs-navbar-item>
       <vs-navbar-item index="3">
-        <router-link to="/knowledge" style="font-size:15px"
+        <router-link to="/knowledge" style="font-size:12px"
           >About Light</router-link
         >
       </vs-navbar-item>
             <vs-navbar-item index="4">
-        <router-link to="/0_testPage" style="font-size:15px"
+        <router-link to="/0_testPage" style="font-size:12px"
           >測試頁面</router-link
         >
       </vs-navbar-item>
@@ -56,14 +55,15 @@
   color:#989BAC;
 }
 #navbar-t{
+  font-family: 'Monoton', cursive;
 }
 
 #logo-t{
-  width:60px;
-  height:60px;
-  background:#0E0F14;
+  width:55px;
+  height:55px;
+  background:rgb(255, 255, 255);
   /* box-shadow:inset 0 0 10px rgb(146, 146, 146); */
-  padding:10px;
+  padding:5px;
   border-radius:50%;
   transition:5s;
 }
